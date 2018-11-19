048f4cccc2dddbfb274455f6d46a24b7bb4462ae
Aleksandr Prianichnikov <pryaniq@gmail.com>
2018-11-20 00:23:10 +0300

    0HrA3gff: Replace window initialization after Storyboard removing

--- TVRemoteControl/Supporting Files/AppDelegate.swift
+++ TVRemoteControl/Supporting Files/AppDelegate.swift
@@ -6,6 +6,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let vc = UIViewController()
+        vc.view.backgroundColor = .lightGray
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+
         return true
     }
 }
