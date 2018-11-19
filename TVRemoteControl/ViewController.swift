ebc4a786d560ab53e58c9e63f8a045f8b18e11ec
Aleksandr Prianichnikov <pryaniq@gmail.com>
2018-11-19 23:38:02 +0300

    0HrA3gff: Create project

---
+++ TVRemoteControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TVRemoteControl
+//
+//  Created by Aleksandr Prianichnikov on 19/11/2018.
+//  Copyright © 2018 robot64. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
