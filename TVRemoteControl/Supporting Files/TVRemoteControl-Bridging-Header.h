940eb63e73771d081b0a86c7934b86752cb1fef1
Daian <daian.aiziatov@gmail.com>
2020-05-03 22:47:26 -0400

    Move MMLANScanner to app folder; Remove redundant imports

--- TVRemoteControl/Supporting Files/TVRemoteControl-Bridging-Header.h
+++ TVRemoteControl/Supporting Files/TVRemoteControl-Bridging-Header.h
@@ -2,11 +2,6 @@
 #define TVRemoteControl_Bridging_Header_h
 
 #import "MMLANScanner.h"
-#import "LANProperties.h"
-#import "PingOperation.h"
-#import "MMLANScanner.h"
-#import "MACOperation.h"
-#import "MacFinder.h"
 #import "MMDevice.h"
 
 #endif /* TVRemoteControl_Bridging_Header_h */
