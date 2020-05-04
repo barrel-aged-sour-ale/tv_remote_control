db465513bf1b58ceeb8acdc2062e4e2e4ab852bf
Aleksandr Prianichnikov <pryaniq@gmail.com>
2020-05-04 14:43:32 +0300

    mPwQlHID: Implementation of LANScanner (v. 0.0.1)
      - remove copyrights
      - fix project file
      - comment out unit tests
      - return MMLanScan lib

--- TVRemoteControl/Services/TVScanner/DataResponseError.swift
+++ TVRemoteControl/Services/TVScanner/DataResponseError.swift
@@ -1,11 +1,3 @@
-//
-//  DataResponseError.swift
-//  RemoteControlPhilips
-//
-//  Created by Daian Aiziatov on 13/01/2019.
-//  Copyright © 2019 Daian Aiziatov. All rights reserved.
-//
-
 import Foundation
 
 enum DataResponseError: Error {
--- TVRemoteControl/Services/TVScanner/TrySystemInfoLoading.swift
+++ TVRemoteControl/Services/TVScanner/TrySystemInfoLoading.swift
@@ -1,11 +1,3 @@
-//
-//  SystemInfoLoading.swift
-//  TVRemoteControl
-//
-//  Created by Daian Aiziatov on 03.05.2020.
-//  Copyright © 2020 robot64. All rights reserved.
-//
-
 import Foundation
 
 protocol TrySystemInfoLoading {
