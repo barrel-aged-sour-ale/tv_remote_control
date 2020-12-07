e95a9b20170ff8b3ac1fc17155ffe6e95db392dd
Daian <daian.aiziatov@gmail.com>
2020-12-06 19:35:50 -0500

    Removed underscore from AnyEncodable

--- TVRemoteControl/Networking/AnyEncodable.swift
+++ TVRemoteControl/Networking/AnyEncodable.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 struct AnyEncodable: Encodable {
-    private let _encode: (Encoder) throws -> Void
+    private let encode: (Encoder) throws -> Void
 
     public init<T: Encodable>(_ wrapped: T) {
-        _encode = wrapped.encode
+        encode = wrapped.encode
     }
 
     func encode(to encoder: Encoder) throws {
-        try _encode(encoder)
+        try encode(encoder)
     }
 }
